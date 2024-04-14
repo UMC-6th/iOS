54cb3331f7b981821a7410ed3e81e7c63885739b
won <andrew112517@naver.com>
2024-04-14 15:41:28 +0900

    [Feat]Week3 프로젝트 세팅

---
+++ Week3/Week3/Week3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week3
+//
+//  Created by 김승원 on 4/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
