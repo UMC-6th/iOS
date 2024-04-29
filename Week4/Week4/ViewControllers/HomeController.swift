cc59418678084ba35a5f76c41cb87723b7b0ad19
won <andrew112517@naver.com>
2024-04-29 14:08:33 +0900

    [Feat]4주차

--- Week4/Week4/ViewControllers/HomeController.swift
+++ Week4/Week4/ViewControllers/HomeController.swift
@@ -12,6 +12,8 @@ class HomeController: UIViewController {
     // MARK: - UI Components
     private let HeaderView = AuthHeaderView(image: "errorImage", title: "로그인 성공!", subTitle: "로그인에 성공하셨습니다.")
     
+    private let logOutButton = CustomButton(title: "로그아웃", hasBackground: true, fontSize: .big)
+    
     private let userLabel: UILabel = {
         let lb = UILabel()
         lb.text = "아이디와 이메일을"
@@ -38,6 +40,8 @@ class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.logOutButton.addTarget(self, action: #selector(didTapLogoutButton), for: .touchUpInside)
         
         setupUI()
         setupConstraints()
@@ -66,14 +70,15 @@ class HomeController: UIViewController {
     // MARK: - UI Setup
     private func setupUI() {
         self.view.backgroundColor = .systemBackground
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "로그아웃",
-                                                                 style: .plain,
-                                                                 target: self,
-                                                                 action: #selector(didTapLogoutButton))
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "로그아웃",
+//                                                                 style: .plain,
+//                                                                 target: self,
+//                                                                 action: #selector(didTapLogoutButton))
         
         self.view.addSubview(HeaderView)
         self.view.addSubview(userLabel)
         self.view.addSubview(emailLabel)
+        self.view.addSubview(logOutButton)
         
     }
     
@@ -89,17 +94,24 @@ class HomeController: UIViewController {
         
         userLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        userLabel.topAnchor.constraint(equalTo: HeaderView.bottomAnchor, constant: 18).isActive = true
+        userLabel.topAnchor.constraint(equalTo: HeaderView.bottomAnchor, constant: 40).isActive = true
         userLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
         userLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
         userLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85).isActive = true
         
         emailLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        emailLabel.topAnchor.constraint(equalTo: userLabel.bottomAnchor, constant: 0).isActive = true
+        emailLabel.topAnchor.constraint(equalTo: userLabel.bottomAnchor, constant: 5).isActive = true
         emailLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
         emailLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
         emailLabel.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85).isActive = true
+        
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        logOutButton.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: -60).isActive = true
+        logOutButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+        logOutButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        logOutButton.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.85).isActive = true
         
     }
     
