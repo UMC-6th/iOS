c8ee8699743e76e14002dcb4a260ac4b077c3db0
won <andrew112517@naver.com>
2024-05-26 15:53:16 +0900

    [Feat]Week7 ScrollView

---
+++ Week7/Week7/Week7App.swift
@@ -0,0 +1,17 @@
+//
+//  Week7App.swift
+//  Week7
+//
+//  Created by 김승원 on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Week7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
